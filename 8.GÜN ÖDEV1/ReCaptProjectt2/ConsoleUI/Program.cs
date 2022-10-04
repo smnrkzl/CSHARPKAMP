4797fef58f0b1fc28e37702ed9e901c9d12a8c27
smnrkzl <96079063+smnrkzl@users.noreply.github.com>
2022-10-04 20:13:40 +0300

    secondcommit

---
+++ 8.GÜN ÖDEV1/ReCaptProjectt2/ConsoleUI/Program.cs
@@ -0,0 +1,15 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        CarManager carManager = new CarManager(new EfCarDal());
+        foreach (var car in carManager.GetAll())
+        {
+            Console.WriteLine(car.Description);
+        }
+        Console.WriteLine("Hello, World!");
+    }
+}
