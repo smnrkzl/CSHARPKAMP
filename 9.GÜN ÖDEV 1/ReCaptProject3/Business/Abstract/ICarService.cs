7d7771b353af795702f7f8915fd1d51060d3fb9d
smnrkzl <96079063+smnrkzl@users.noreply.github.com>
2022-10-04 22:40:48 +0300

    firstcommitt

---
+++ 9.GÜN ÖDEV 1/ReCaptProject3/Business/Abstract/ICarService.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Concrete;
+using Entities.IDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface ICarService
+    {
+        List<CarDetailDto> GetAll();
+        void Add(Car car);
+    }
+}
---
+++ 9.GÜN ÖDEV 1/ReCaptProject3/ConsoleUI/Program.cs
@@ -0,0 +1,16 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        CarManager carManager = new CarManager(new EfCarDal());
+        foreach (var car in carManager.GetAll())
+        {
+            Console.WriteLine(car.CarName + " "  + " " +car.ModelYear + car.BrandName);
+        }
+
+        Console.WriteLine("Hello, World!");
+    }
+}
