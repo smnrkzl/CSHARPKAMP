fdfb8e1d3cd47379e6cd886705c74176a23342f9
smnrkzl <96079063+smnrkzl@users.noreply.github.com>
2022-10-14 17:54:01 +0300

    ödev1

---
+++ 10.GÜN ÖDEVLER/Ödev 1/ReCaptProject4/Business/Concrete/ColorManager.cs
@@ -0,0 +1,55 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Result;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class ColorManager : IColorService
+    {
+        IColorDal _colorDal;
+        public ColorManager(IColorDal colorDal)
+        {
+            _colorDal = colorDal;
+        }
+
+
+        public IResult Add(Color color)
+        {
+            if (color.ColorName!.Length < 5)
+            {
+                return new ErrorResult(Message.ColorNameInvalid);
+            }
+            _colorDal.Add(color);
+            return new SuccessResult(Message.ColorAdded);
+        }
+
+        public IResult Delete(Color color)
+        {
+            _colorDal.Delete(color);
+            return new SuccessResult(Message.ColorDeleted);
+        }
+
+        public IDataResult<List<Color>> GetAll()
+        {
+            return new SuccessDataResult<List<Color>>(_colorDal.GetAll());
+        }
+
+        public IDataResult<Color> GetCarsByColorId()
+        {
+            return new SuccessDataResult<Color>(_colorDal.Get(p => p.Id == colorId));
+        }
+
+        public IResult Update(Color color)
+        {
+            _colorDal.Update(color);
+            return new SuccessResult(Message.ColorUpdated);
+        }
+    }
+}
---
+++ 10.GÜN ÖDEVLER/Ödev 1/ReCaptProject4/DataAccess/Concrete/EntityFramework/CarContext.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class CarContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLOCALDB;Database=CARS;Integrated Security=True");
+
+        }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Color> Colors { get; set; }
+      }
+}
