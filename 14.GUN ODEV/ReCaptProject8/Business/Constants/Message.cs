69744fccfdfbcc4d9119b031f0fb6df051c75efe
smnrkzl <kizilsemanur@gmail.com>
2022-11-07 23:10:42 +0300

    JWT

---
+++ 14.GUN ODEV/ReCaptProject8/Business/Constants/Message.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    public static class Message
+    {
+        public static string ProductAdded = "Ürün eklendi";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz";
+        public static string MaintenanceTime = "Sistem bakımda";
+        public static string ProductListed = "Ürünler listelendi";
+        public static string ProductDeleted = "Ürün silindi";
+        public static string ProductUpdated = "Ürün güncellendi";
+        public static string BrandNameInvalid = "Marka ismi geçersiz";
+        public static string BrandAdded = "Marka eklendi";
+        public static string BrandDeleted = "Marka silindi";
+        public static string BrandUpdated = "Marka güncellendi";
+        public static string BrandListed = "Marka listelendi";
+        public static string ColorNameInvalid = "Renk ismi geçersiz";
+        public static string ColorAdded = "Renk eklendi";
+        public static string ColorDeleted = "Renk silindi";
+        public static string ColorUpdated = "Renk güncellendi";
+        public static string UserNameInvalid = "Kullanıcı ismi geçersiz";
+        public static string UserAdded = "Kullanıcı eklendi";
+        public static string CustomerAdded = "Müşteri eklendi";
+        public static string RentalAdded = "Kiralama kaydı eklendi";
+        public static string InvalidRental = " Kiralanacak araba müsait değil";
+        public static string UserNotFound = "User not found.";
+        public static string PasswordError = "Password error.";
+        public static string SuccessfulyLogin = "Successfuly login";
+        public static string UserAlreadyExist = "User already exist";
+        public static string UserRegistered = "User registered.";
+        public static string AccessTokenCreated = "Access token created";
+    }
+}
---
+++ 14.GUN ODEV/ReCaptProject8/Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CarValidator:AbstractValidator<Car>
+    {
+        public CarValidator()
+        {           
+            RuleFor(c => c.ModelYear).NotEmpty().WithMessage("Yıl bilgisi boş olamaz");
+            RuleFor(c => c.CarName).NotEmpty().WithMessage("Araç ismi boş olamaz!");
+            RuleFor(c => c.CarName).MinimumLength(2).WithMessage("Araç ismi 5'den az olamaz!");
+            RuleFor(c => c.Description).NotEmpty().WithMessage("Açıklama boş olamaz");
+        }
+    }
+}
